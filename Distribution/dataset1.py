be4f5461db320da93ae04ce7f426f93bf663d270
eduvedras <edu.vedras@gmail.com>
2022-11-27 11:05:01 +0000

    distribution

--- Distribution/dataset1.py
+++ Distribution/dataset1.py
@@ -1,8 +1,12 @@
 #%%
 import pandas as pd
 from matplotlib.pyplot import figure, savefig, show
-from ds_charts import get_variable_types,choose_grid,multiple_bar_chart, HEIGHT
-from matplotlib.pyplot import subplots, savefig, show
+from ds_charts import get_variable_types,multiple_line_chart,choose_grid,multiple_bar_chart, HEIGHT
+from matplotlib.pyplot import subplots, savefig, show, Axes
+from seaborn import distplot
+from pandas import Series
+from scipy.stats import norm, expon, lognorm
+from numpy import log
 
 filename = '../diabetic_data.csv'
 data = pd.read_csv(filename)
@@ -25,11 +29,11 @@ savefig('imageD1/numeric_single_boxplots.png')
 show()
 
 
+
 NR_STDEV: int = 2
 
 numeric_vars = get_variable_types(data)['Numeric']
-print(numeric_vars)
-print("--------")
+
 if [] == numeric_vars:
     raise ValueError('There are no numeric variables.')
 
@@ -52,5 +56,71 @@ outliers = {'iqr': outliers_iqr, 'stdev': outliers_stdev}
 figure(figsize=(12, HEIGHT))
 multiple_bar_chart(numeric_vars, outliers, title='Nr of outliers per variable', xlabel='variables', ylabel='nr outliers', percentage=False)
 savefig('imageD1/outliers.png')
+show()
+
+
+
+numeric_vars = get_variable_types(data)['Numeric']
+if [] == numeric_vars:
+    raise ValueError('There are no numeric variables.')
+
+fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT, rows*HEIGHT), squeeze=False)
+i, j = 0, 0
+for n in range(len(numeric_vars)):
+    axs[i, j].set_title('Histogram for %s'%numeric_vars[n])
+    axs[i, j].set_xlabel(numeric_vars[n])
+    axs[i, j].set_ylabel("nr records")
+    axs[i, j].hist(data[numeric_vars[n]].dropna().values, 'auto')
+    i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+savefig('imageD1/single_histograms_numeric.png')
+show()
+
+
+'''
+numeric_vars = get_variable_types(data)['Numeric']
+if [] == numeric_vars:
+    raise ValueError('There are no numeric variables.')
+
+fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT, rows*HEIGHT), squeeze=False)
+i, j = 0, 0
+for n in range(len(numeric_vars)):
+    axs[i, j].set_title('Histogram with trend for %s'%numeric_vars[n])
+    distplot(data[numeric_vars[n]].dropna().values, norm_hist=True, ax=axs[i, j], axlabel=numeric_vars[n])
+    i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+savefig('imageD1/histograms_trend_numeric.png')
+show()'''
+
+
+
+def compute_known_distributions(x_values: list) -> dict:
+    distributions = dict()
+    # Gaussian
+    mean, sigma = norm.fit(x_values)
+    distributions['Normal(%.1f,%.2f)'%(mean,sigma)] = norm.pdf(x_values, mean, sigma)
+    # Exponential
+    loc, scale = expon.fit(x_values)
+    distributions['Exp(%.2f)'%(1/scale)] = expon.pdf(x_values, loc, scale)
+    # LogNorm
+    sigma, loc, scale = lognorm.fit(x_values)
+    distributions['LogNor(%.1f,%.2f)'%(log(scale),sigma)] = lognorm.pdf(x_values, sigma, loc, scale)
+    return distributions
+
+def histogram_with_distributions(ax: Axes, series: Series, var: str):
+    values = series.sort_values().values
+    ax.hist(values, 20, density=True)
+    distributions = compute_known_distributions(values)
+    multiple_line_chart(values, distributions, ax=ax, title='Best fit for %s'%var, xlabel=var, ylabel='')
+
+numeric_vars = get_variable_types(data)['Numeric']
+if [] == numeric_vars:
+    raise ValueError('There are no numeric variables.')
+
+fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT, rows*HEIGHT), squeeze=False)
+i, j = 0, 0
+for n in range(len(numeric_vars)):
+    histogram_with_distributions(axs[i, j], data[numeric_vars[n]].dropna(), numeric_vars[n])
+    i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+    print(numeric_vars[n])
+savefig('imageD1/histogram_numeric_distribution.png')
 show()
 # %%
